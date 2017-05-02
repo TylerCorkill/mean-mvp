0999e4e7500ba7d650dc48ccd74bf4b98484420c
TylerCorkill <TylerCorkill@gmail.com>
2017-05-01 17:30:22 -0700

    refactored db helpers outside of routes

--- routes.js
+++ routes.js
@@ -1,20 +1,11 @@
 var express = require('express');
 var path = require("path");
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'ratings'
-});
-
+var db = require('./helpers/db');
 
 var viewPath = path.join(__dirname+'/public/views/');
 var app = express();
 
-// app.set('view engine', 'pug');
-// app.set('views', './views');
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 
@@ -29,17 +20,20 @@ app.get('/', function(req, res) {
 
 app.post('/submit', function(req, res) {
   console.log(req.body);
-  res.sendFile(viewPath + 'index.html');
+  res.redirect('/');
+  // res.sendFile(viewPath + 'index.html');
 });
 
 app.post('/signup', function(req, res) {
   console.log(req.body);
-  res.sendFile(viewPath + 'index.html');
+  res.redirect('/');
+  // res.sendFile(viewPath + 'index.html');
 });
 
 app.post('/login', function(req, res) {
   console.log(req.body);
-  res.sendFile(viewPath + 'index.html');
+  res.redirect('/');
+  // res.sendFile(viewPath + 'index.html');
 });
 
 
